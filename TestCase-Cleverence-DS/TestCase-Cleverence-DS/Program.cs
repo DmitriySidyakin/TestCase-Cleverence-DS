4832ae91413f5deabd76cf25b58b669da1870efa
Dmitriy Sidyakin <sidyakind@gmail.com>
2023-06-02 10:52:23 +0300

    Сделал Task 1

--- TestCase-Cleverence-DS/TestCase-Cleverence-DS/Program.cs
+++ TestCase-Cleverence-DS/TestCase-Cleverence-DS/Program.cs
@@ -1,2 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using TestCase_Cleverence_DS;
+
+Console.WriteLine("Test 1");
+
+string s1 = "aaabbcccdde";
+Console.WriteLine($"BASE = {s1} ; RESULT = {Task1.Compression(s1)}");
--- TestCase-Cleverence-DS/TestCase-Cleverence-DS/Task1.cs
+++ TestCase-Cleverence-DS/TestCase-Cleverence-DS/Task1.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,5 +9,35 @@ namespace TestCase_Cleverence_DS
 {
     internal class Task1
     {
+
+        public static string Compression(string s)
+        {
+            if(string.IsNullOrEmpty(s))
+            {
+                return s;
+            }
+
+            string result = string.Empty;
+
+            char? prevChar = s[0];
+            int startIndex = 1;
+            for(int i = 1; i < s.Length; i++)
+            { 
+                if(prevChar == s[i])
+                {
+                    startIndex++;
+                }
+                else
+                {
+                    result += startIndex == 1 ? prevChar : $"{prevChar}{startIndex}";
+                    prevChar = s[i];
+                    startIndex = 1;
+                }
+            }
+
+            result += startIndex == 1 ? prevChar : $"{prevChar}{startIndex}";
+
+            return result;
+        }
     }
 }
