12c98e009851cd9fcaa86e93570ad1579835ac3d
Dmitriy Sidyakin <sidyakind@gmail.com>
2023-06-02 12:03:33 +0300

    Сделал Task 2

--- TestCase-Cleverence-DS/TestCase-Cleverence-DS/Program.cs
+++ TestCase-Cleverence-DS/TestCase-Cleverence-DS/Program.cs
@@ -5,3 +5,32 @@ Console.WriteLine("Test 1");
 
 string s1 = "aaabbcccdde";
 Console.WriteLine($"BASE = {s1} ; RESULT = {Task1.Compression(s1)}");
+
+Console.WriteLine("Test 2");
+
+int[,] matrix1 = new int[3, 3] {
+    {1, 2, 3 },
+    {4, 5, 6 },
+    {7, 8, 9 }
+};
+
+int[,] matrix2 = new int[3, 4] {
+    {1, 2, 3, 4 },
+    {5, 6, 7, 8 },
+    {9, 10, 11, 12 }
+};
+
+int[] result1 = Task2.Spiralize(3, 3, matrix1);
+int[] result2 = Task2.Spiralize(4, 3, matrix2);
+
+for(int i = 0; i < result1.Length; i++)
+{
+    Console.Write(result1[i] + (i == result1.Length - 1 ? ";" : ", "));
+}
+
+Console.WriteLine();
+
+for (int i = 0; i < result2.Length; i++)
+{
+    Console.Write(result2[i] + (i == result2.Length - 1 ? ";" : ", "));
+}
---
+++ TestCase-Cleverence-DS/TestCase-Cleverence-DS/Task2.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestCase_Cleverence_DS
+{
+    internal class Task2
+    {
+        /*
+         int[,] matrix1 = new int[3, 3] {
+            {1, 2, 3 },
+            {4, 5, 6 },
+            {7, 8, 9 }
+        };
+         */
+        public static int[] Spiralize(int width, int height, int[,] matrix)
+        {
+            if (matrix.Length == 0)
+                return new int[0];
+
+            int resultSize = matrix.Length;
+
+            int[] result = new int[resultSize];
+
+            bool isItTheEnd = false;
+            int startTopIndex_i = 0;
+            int endTopIndex_i = height - 1;
+            int startTopIndex_j = 0;
+            int endTopIndex_j = width - 1;
+            int currentIndex = 0;
+            while (!isItTheEnd)
+            {
+                for (int i = startTopIndex_i; i <= endTopIndex_i && currentIndex != resultSize; i++)
+                {
+                    result[currentIndex] = matrix[i, startTopIndex_j];
+                    currentIndex++;
+                }
+
+                startTopIndex_j++;
+
+                for (int j = startTopIndex_j; j <= endTopIndex_j && currentIndex != resultSize; j++)
+                {
+                    result[currentIndex] = matrix[endTopIndex_i, j];
+                    currentIndex++;
+                }
+
+                endTopIndex_i--;
+
+                for (int i = endTopIndex_i; i >= startTopIndex_i && currentIndex != resultSize; i--)
+                {
+                    result[currentIndex] = matrix[i, endTopIndex_j];
+                    currentIndex++;
+                }
+
+                endTopIndex_j--;
+
+                for (int j = endTopIndex_j; j >= startTopIndex_j && currentIndex != resultSize; j--)
+                {
+                    result[currentIndex] = matrix[startTopIndex_i, j];
+                    currentIndex++;
+                }
+
+                startTopIndex_i++;
+
+                if (currentIndex == resultSize)
+                {
+                    isItTheEnd = true;
+                }
+            }
+
+            return result;
+        }
+    }
+}
